030fe0f22d943fd5597c80eb8605d7f8761dc5c9
Chibuike <161600816+ChibuikeOluigbo@users.noreply.github.com>
2024-03-21 10:54:12 +0000

    Ex4

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,7 +1,7 @@
 # Exercise 4 -> Correct
 # Your solution comes here
 
-num = input("Enter a 4 digit number: ")
+# num = input("Enter a 4 digit number: ")
 num = input()
 
 if num[0] == num[3] and num[1] == num[2]:
