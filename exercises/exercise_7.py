2947146fd68690049f1bb404cf4de2f8409684df
Chibuike <161600816+ChibuikeOluigbo@users.noreply.github.com>
2024-03-21 10:56:54 +0000

    7 and 11

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,8 +1,8 @@
 # Exercise 7 -> Correct
 # Your solution comes here
 
-num = input("Enter a 4 digit number: ")
-# num = input()
+# num = input("Enter a 4 digit number: ")
+num = input()
 
 sum1 = int(num[0]) + int(num[1]) + int(num[2]) + int(num[3])
 sum = str(sum1)
