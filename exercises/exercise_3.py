fee29a9c18f3f865e268474d085e0014ddf755a8
Chibuike <161600816+ChibuikeOluigbo@users.noreply.github.com>
2024-03-25 12:07:31 +0000

    3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,4 +1,4 @@
-# Exercise 3 -> Incorrect
+# Exercise 3 -> Correct
 # Your solution comes here
 
 time = int(input("Enter the number of seconds: "))
@@ -7,6 +7,8 @@ time = int(input("Enter the number of seconds: "))
 hours = (time // 3600) % 24
 minutes = (time % 3600) // 60
 seconds = (time % 3600) % 60
+
+
 
 clock = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
 print(clock)
